5715afa1da8f279b2f4fcf7cc1840d82eb7a11d6
cayomol01 <jacmg01@gmail.com>
2024-11-15 08:57:36 -0600

    prueba

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("hola")
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description="Procesar argumentos en Docker")
+    parser.add_argument("--nombre", type=str, help="Nombre del usuario", required=True)
+    parser.add_argument("--edad", type=int, help="Edad del usuario", required=True)
+    args = parser.parse_args()
+
+    print(f"Hola {args.nombre}, tienes {args.edad} años.")
+
+if __name__ == "__main__":
+    main()
