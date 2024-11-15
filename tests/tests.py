244c799743d7d2650f6b1a56a8d25a9f5e61234e
cayomol01 <jacmg01@gmail.com>
2024-11-15 11:42:48 -0600

    cambios

--- tests/tests.py
+++ tests/tests.py
@@ -6,6 +6,6 @@ class TestFunctions(unittest.TestCase):
     def test_add(self):
         self.assertEqual(prueba(2, 3), 6)
         self.assertEqual(prueba(-1, 1), -1)
-        
+
 if __name__ == "__main__":
     unittest.main()
