31489e0739747b16d13c88073e246ff2d0df54d9
cayomol01 <jacmg01@gmail.com>
2024-11-23 23:47:58 -0600

    tests prueba

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,18 @@
+# Usa Python 3.10 como base
 FROM python:3.10
 
-ADD main.py .
+# Establecer el directorio de trabajo en el contenedor
+WORKDIR /app
 
-CMD ["python", "./main.py"]
+# Copiar los archivos necesarios al contenedor
+ADD main.py .
+ADD models/ ./models/  
+ADD data/ ./data/      
+
+# Instalar dependencias necesarias
+# Si usas un archivo requirements.txt, agrégalo y ejecuta pip install
+# ADD requirements.txt .
+# RUN pip install --no-cache-dir -r requirements.txt
+
+# Configurar un comando interactivo
+CMD ["python", "-i", "./main.py"]
--- tests/tests.py
+++ tests/tests.py
@@ -1,11 +1,26 @@
 import unittest
-from model import prueba
+from model import *
+import joblib
+import numpy as np
+import pandas as pd
 
 
 class TestFunctions(unittest.TestCase):
-    def test_add(self):
-        self.assertEqual(prueba(2, 3), 6)
-        self.assertEqual(prueba(-1, 1), -1)
+
+    def testMetric(self):
+        model = load_model('Models/modelV1.pkl')
+        df = load_data('MarathonData.csv')
+
+        df = preprocess_data(df)
+        X = df[['km4week', 'sp4week', 'Wall21', 'Score']].values
+        y = df['MarathonTime'].values
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+        y_pred = model.predict(X_test)
+        mse = mean_squared_error(y_test, y_pred)
+
+        self.assertTrue(mse<=0.05)
+        
 
 if __name__ == "__main__":
     unittest.main()
